1c0bf53ddc89b32cb56a7dfaed66043563b37a2c
link151 <link151@163.com>
2018-05-22 19:14:27 +0800

    Autowire

--- src/seu/edu/cn/link/Triangle.java
+++ src/seu/edu/cn/link/Triangle.java
@@ -4,22 +4,52 @@ import java.util.List;
 
 public class Triangle {
 
-	List<Point> points;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	
 
 
-	public List<Point> getPoints() {
-		return points;
+	public Point getPointA() {
+		return pointA;
 	}
 
 
-	public void setPoints(List<Point> points) {
-		this.points = points;
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
+
+
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
 
 
 	public void draw() {
-		for(Point point : points){
-			System.out.println("point: (" + point.getX() + ", " + point.getY() + ")");
-		}
+		
+		System.out.println("pointA: (" + getPointA().getX() + ", " + getPointA().getY() + ")");
+		System.out.println("pointB: (" + getPointB().getX() + ", " + getPointB().getY() + ")");
+		System.out.println("pointC: (" + getPointC().getX() + ", " + getPointC().getY() + ")");
 	}
 }
