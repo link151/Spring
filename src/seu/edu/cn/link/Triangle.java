1cb41f472e4cd6b0ebe68cf18b0a8b50129e331d
link151 <link151@163.com>
2018-05-22 15:29:11 +0800

    引用其他对象，对象嵌套

--- src/seu/edu/cn/link/Triangle.java
+++ src/seu/edu/cn/link/Triangle.java
@@ -2,40 +2,44 @@ package seu.edu.cn.link;
 
 public class Triangle {
 
-	private String type;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 	
-	private int height;
-	
-	Triangle(String type){
-		this.type = type;
-	}
-	
-	Triangle(int height){
-		this.height = height;
-	}
-	
-	Triangle(String type, int height){
-		this.type = type;
-		this.height = height;
-	}
-	
-	public int getHeight() {
-		return height;
+
+	public Point getPointA() {
+		return pointA;
 	}
 
-//	public void setHeight(int height) {
-//		this.height = height;
-//	}
 
-	public String getType() {
-		return type;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
 
-//	public void setType(String type) {
-//		this.type = type;
-//	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
 
 	public void draw() {
-		System.out.println(getType() + " Triangle of height " + getHeight());
+		System.out.println("pointA: (" + getPointA().getX() + ", " + getPointA().getY() + ")");
+		System.out.println("pointB: (" + getPointB().getX() + ", " + getPointB().getY() + ")");
+		System.out.println("pointC: (" + getPointC().getX() + ", " + getPointC().getY() + ")");
 	}
 }
