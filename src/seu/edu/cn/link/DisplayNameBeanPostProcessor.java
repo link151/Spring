9d75f0dc74fb0934992a3ce5ecd485918d6deb4a
link151 <link151@163.com>
2018-05-26 22:45:03 +0800

    使用BeanPostProcessor 在bean初始化前后添加统一的处理

---
+++ src/seu/edu/cn/link/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,22 @@
+package seu.edu.cn.link;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor{
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName)
+			throws BeansException {
+		System.out.println("After Initialization bean name is " + beanName);
+		return bean;
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName)
+			throws BeansException {
+		System.out.println("Before Initialization bean name is " + beanName);
+		return bean;
+	}
+
+}
