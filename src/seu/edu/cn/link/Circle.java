29646023f6a99beef66653cca5e5671055fcb8ce
link151 <link151@163.com>
2018-05-27 17:38:09 +0800

    Using MessageSource To Get Text From Property Files

--- src/seu/edu/cn/link/Circle.java
+++ src/seu/edu/cn/link/Circle.java
@@ -4,11 +4,15 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
 @Component("shape")
 public class Circle implements Shape{
 	private Point center;
+	@Autowired
+	private MessageSource messageSource;
 
 	public Point getCenter() {
 		return center;
@@ -19,10 +23,19 @@ public class Circle implements Shape{
 		this.center = center;
 	}
 
+	public MessageSource getMessageSource() {
+		return messageSource;
+	}
+
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+
 	@Override
 	public void draw() {
-		System.out.println("Draw Circle");
-		System.out.println("center point: (" + center.getX() + ", " + center.getY() + ")");
+		System.out.println(messageSource.getMessage("drawing.circle", null, "***default drawing circle***", null));
+		System.out.println(messageSource.getMessage("drawing.point", new Object[] {center.getX(), center.getY()}, "***default drawing point***", null));
+		System.out.println(messageSource.getMessage("greeting", null, "***default greeting***", null));
 	}
 	
 	@PostConstruct
--- src/seu/edu/cn/link/DrawApplication.java
+++ src/seu/edu/cn/link/DrawApplication.java
@@ -2,6 +2,8 @@ package seu.edu.cn.link;
 
 import java.io.File;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -17,11 +19,16 @@ public class DrawApplication {
 //		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
 //		Triangle triangle = (Triangle) factory.getBean("triangle");
 
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		context.registerShutdownHook();
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//		context.registerShutdownHook();
 		Shape shape = (Shape) context.getBean("shape");
 		shape.draw();
-		
+		//首先获取bean，再使用bean获取message，默认的是根据name获取bean 【messageSource】
+//		MessageSource sourceMessage = (MessageSource) context.getBean("sourceMessage"); 
+//		System.out.println(sourceMessage.getMessage("greeting", null, "***default greeting***", null));
+
+		//默认的是根据name获取bean 【messageSource】
+//		System.out.println(context.getMessage("greeting", null, "***default greeting***", null));
 		
 	}
 
